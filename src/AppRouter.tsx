dd93fdf4ada9307a4b3605c8c85cd5d9d34e31e9
Johannes <1107jjs@web.de>
2026-01-19 15:11:40 +0100

    fix routes and its inputs

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import WorldHeatmap from "./components/WorldHeatMap";
 import StatisticsPage from "./pages/StatisticsPage";
@@ -12,14 +12,13 @@ type AppRouterProps = {
 
 export default function AppRouter({ markElement, mapsElement, statisticsElement }: AppRouterProps) {
   return (
-  <HashRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={markElement} />
-      <Route path="/maps" element={mapsElement} />
-      <Route path="/statistics" element={statisticsElement} />
-    </Routes>
-  </HashRouter>
-
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={markElement} />
+        <Route path="/maps" element={mapsElement ?? <div style={{ padding: 24 }}><h2>Maps</h2><WorldHeatmap /></div>} />
+        <Route path="/statistics" element={statisticsElement ?? <StatisticsPage />} />
+      </Routes>
+    </HashRouter>
   );
 }
