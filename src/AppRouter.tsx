d01e4d22d17c4f9004511578a74b176bc757c75f
Johannes <1107jjs@web.de>
2026-01-19 15:12:16 +0100

    fix imports

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import WorldHeatmap from "./components/WorldHeatMap";
 import StatisticsPage from "./pages/StatisticsPage";
